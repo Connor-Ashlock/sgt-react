5af2e76c51736a88019fb0a084e1c7c135ff1e41
connor-ashlock <connor9804@yahoo.com>
2020-09-21 23:23:06 +0000

    changed component back to a class and added a createTableRow method

--- client/components/grade-table.jsx
+++ client/components/grade-table.jsx
@@ -1,39 +1,49 @@
 import React from 'react';
 
-function GradeTable(props) {
-  const grades = props.grades;
-  return (
-    <div className="row">
-      <div className="col">
-        <table className="border border-dark table table-striped table-hover">
-          <thead className="thead-dark">
-            <tr>
-              <th className="border-right-black">Student Name</th>
-              <th className="border-right-black">Course</th>
-              <th>Grade</th>
-            </tr>
-          </thead>
-          <tbody>
-            <Grade grades={grades} />
-          </tbody>
-        </table>
-        {!grades.length && 'No grades recorded'}
+class GradeTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.createTableRows = this.createTableRows.bind(this);
+  }
+
+  createTableRows() {
+    return <>{this.props.grades.map(student => <Grade key={student.id} student={student}/>)}</>;
+  }
+
+  render() {
+    const rows = this.createTableRows();
+    const grades = this.props.grades;
+    return (
+      <div className="row">
+        <div className="col">
+          <table className="border border-dark table table-striped table-hover">
+            <thead className="thead-dark">
+              <tr>
+                <th className="border-right-black">Student Name</th>
+                <th className="border-right-black">Course</th>
+                <th>Grade</th>
+              </tr>
+            </thead>
+            <tbody>
+              { rows }
+            </tbody>
+          </table>
+          {!grades.length && 'No grades recorded'}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 function Grade(props) {
-  const tr = props.grades.map(student => {
-    return (
-      <tr key={student.id}>
-        <td className="border-right-black">{student.name}</td>
-        <td className="border-right-black">{student.course}</td>
-        <td>{student.grade}</td>
-      </tr>
-    );
-  });
-  return tr;
+  const student = props.student;
+  return (
+    <tr>
+      <td className="border-right-black">{student.name}</td>
+      <td className="border-right-black">{student.course}</td>
+      <td>{student.grade}</td>
+    </tr>
+  );
 }
 
 export default GradeTable;
