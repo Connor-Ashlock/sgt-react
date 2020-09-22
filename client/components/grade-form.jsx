4f40ab0ac3fa2318185984684fd8a16ee2f07caf
connor-ashlock <connor9804@yahoo.com>
2020-09-22 03:04:23 +0000

    added handle change methods for the grade form inputs

--- client/components/grade-form.jsx
+++ client/components/grade-form.jsx
@@ -1,6 +1,26 @@
 import React from 'react';
 
 class GradeForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleCourseChange = this.handleCourseChange.bind(this);
+    this.handleGradeChange = this.handleGradeChange.bind(this);
+    this.state = { studentName: '', studentCourse: '', studentGrade: '' };
+  }
+
+  handleNameChange(ev) {
+    this.setState({ studentName: ev.target.value });
+  }
+
+  handleCourseChange(ev) {
+    this.setState({ studentCourse: ev.target.value });
+  }
+
+  handleGradeChange(ev) {
+    this.setState({ studentGrade: ev.target.value });
+  }
+
   render() {
     return (
       <form className="col-lg-4">
@@ -8,19 +28,19 @@ class GradeForm extends React.Component {
           <div className="input-group-prepend">
             <span className="input-group-text"><i className="fas fa-user"></i></span>
           </div>
-          <input type="text" className="col" name="name" placeholder="Name" />
+          <input type="text" value={this.state.studentName} onChange={this.handleNameChange} className="col" name="name" placeholder="Name" />
         </div>
         <div className="input-group mb-3">
           <div className="input-group-prepend">
             <span className="input-group-text"><i className="fas fa-list"></i></span>
           </div>
-          <input type="text" className="col" name="course" placeholder="Course" />
+          <input type="text" value={this.state.studentCourse} onChange={this.handleCourseChange} className="col" name="course" placeholder="Course" />
         </div>
         <div className="input-group mb-3">
           <div className="input-group-prepend">
             <span className="input-group-text"><i className="fas fa-graduation-cap"></i></span>
           </div>
-          <input type="text" className="col" name="grade" placeholder="Grade" />
+          <input type="text" value={this.state.studentGrade} onChange={this.handleGradeChange} className="col" name="grade" placeholder="Grade" />
         </div>
         <div className="input-group justify-content-end mb-3">
           <button type="submit" className="mx-3 btn btn-success">Add</button>
